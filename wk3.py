51c7b08a2a75aab28b74ef8b117341d34eb47f58
mikhail-cct <tayemnyj+cct@gmail.com>
2021-01-26 20:08:41 +0000

    wk3 changes

---
+++ wk3.py
@@ -0,0 +1,13 @@
+# sum = 5 + 3 # 8
+
+# sum_2 = 2 + sum # 10
+
+# print(sum_2) # Prints: 10
+
+my_list = [1,2,3,4] # Initialize my_list
+
+my_list_2 = [5,6,7,8] # Initialize my_list_2
+
+my_list = my_list + my_list_2 # Take the current value of my_list and add my_list_2 to it
+
+print(my_list) # Prints: [1, 2, 3, 4, 5, 6, 7, 8]
